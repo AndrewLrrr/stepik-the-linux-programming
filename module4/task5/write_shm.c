c4ee6a738af8429bae7f4dcb6487adf29f6dde57
Andrey larin <larin.projects@gmail.com>
2019-04-26 12:48:00 +0300

    A little refactoring of Module 4 shm writer

--- module4/task5/write_shm.c
+++ module4/task5/write_shm.c
@@ -4,7 +4,8 @@
 #include <sys/shm.h>
 #include <sys/stat.h>
 
-void create_shared_memory(key_t shm_key) {
+void create_shared_memory(key_t shm_key)
+{
     const int SHM_SIZE = 1000;
 
     int shm_id = shmget(shm_key, SHM_SIZE, IPC_CREAT | S_IRUSR | S_IWUSR);
@@ -22,7 +23,8 @@ void create_shared_memory(key_t shm_key) {
     }
 }
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
     key_t shm_key_1 = atoi(argv[1]);
     key_t shm_key_2 = atoi(argv[2]);
   
